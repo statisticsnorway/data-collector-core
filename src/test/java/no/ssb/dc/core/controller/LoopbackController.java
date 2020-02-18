c29cb0d4d54fadcb89e381d28042c3deab74abd2
oranheim <oranheim@gmail.com>
2020-02-18 11:15:47 +0100

    Refactoring of server-base packages

--- src/test/java/no/ssb/dc/core/controller/LoopbackController.java
+++ src/test/java/no/ssb/dc/core/controller/LoopbackController.java
@@ -6,7 +6,7 @@ import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 import no.ssb.dc.api.http.Request;
 import no.ssb.dc.api.util.JsonParser;
-import no.ssb.dc.application.Controller;
+import no.ssb.dc.application.server.Controller;
 
 import java.util.Set;
 
