e0ca2355bff81968993352e54731e9851218fc49
oranheim <oranheim@gmail.com>
2020-06-03 19:08:57 +0200

    Support for Put and Delete methods

---
+++ src/main/java/no/ssb/dc/core/handler/DeleteHandler.java
@@ -0,0 +1,33 @@
+package no.ssb.dc.core.handler;
+
+import no.ssb.dc.api.context.ExecutionContext;
+import no.ssb.dc.api.handler.Handler;
+import no.ssb.dc.api.http.Request;
+import no.ssb.dc.api.http.Response;
+import no.ssb.dc.api.node.Delete;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("unchecked")
+@Handler(forClass = Delete.class)
+public class DeleteHandler extends AbstractOperationHandler<Delete> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DeleteHandler.class);
+
+    public DeleteHandler(Delete node) {
+        super(node);
+    }
+
+    @Override
+    public ExecutionContext execute(ExecutionContext input) {
+        super.execute(input);
+        int requestTimeout = beforeRequest(input);
+
+        Request.Builder requestBuilder = Request.newRequestBuilder().DELETE(); // . timeout(Duration.ofSeconds(requestTimeout));
+
+        Response response = doRequest(input, requestTimeout, requestBuilder);
+
+        return handleResponse(input, response);
+    }
+
+}
